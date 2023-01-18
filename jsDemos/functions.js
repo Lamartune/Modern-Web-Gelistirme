490daf5cb7e56c1a40c676f1063c76e78a00cf06
Lamartune <fatih.tun.2002@gmail.com>
2023-01-18 08:57:05 +0300

    sayiUret Fonksiyonu yazildi.

--- jsDemos/functions.js
+++ jsDemos/functions.js
@@ -1,8 +1,16 @@
-var sayi1 = Math.ceil(Math.random() * 49)
-var sayi2 = Math.ceil(Math.random() * 49)
-var sayi3 = Math.ceil(Math.random() * 49)
-var sayi4 = Math.ceil(Math.random() * 49)
-var sayi5 = Math.ceil(Math.random() * 49)
-var sayi6 = Math.ceil(Math.random() * 49)
+function sayiUret(){
+    return Math.ceil(Math.random() * 49)
+}
+
+//Keyword = return
+
+sayiUret()
+
+var sayi1 = sayiUret()
+var sayi2 = sayiUret()
+var sayi3 = sayiUret()
+var sayi4 = sayiUret()
+var sayi5 = sayiUret()
+var sayi6 = sayiUret()
 
 console.log('Kolon: ', sayi1 + ' '+sayi2 + ' '+sayi3 + ' '+sayi4 + ' '+sayi5 + ' '+sayi6 + ' ')
