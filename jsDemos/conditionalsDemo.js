e75677528b9ddbbad5825262f46d054a519ef61d
Lamartune <fatih.tun.2002@gmail.com>
2023-01-19 10:06:36 +0300

    En buyuk sayiyi bulma uygulamasi yapildi.

---
+++ jsDemos/conditionalsDemo.js
@@ -0,0 +1,17 @@
+var sayi1 = 29
+var sayi2 = 30
+var sayi3 = 3
+
+//En büyük sayıyı bulunuz
+
+var enBuyuk = sayi1 //29
+
+if(enBuyuk<sayi2){
+    enBuyuk = sayi2 //30
+}
+
+if(enBuyuk<sayi3){
+    enBuyuk=sayi3
+}
+
+console.log('En büyük sayı: '+ enBuyuk)
