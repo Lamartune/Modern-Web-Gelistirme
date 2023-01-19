cd68b1ad58583f36e139ebd3564f4d2b66ba9bfa
Lamartune <fatih.tun.2002@gmail.com>
2023-01-19 10:34:02 +0300

    Ic Ice Donguler tamamlandi.

---
+++ jsDemos/loopDemo.js
@@ -0,0 +1,10 @@
+function sayiUret(ustLimit = 49) {
+  return Math.ceil(Math.random() * ustLimit);
+}
+
+for (var i = 1; i <= 6; i++) {
+    console.log(i +".KOLON")
+  for (var j = 1; j <= 6; j++) {
+    console.log(sayiUret());
+  }
+}
