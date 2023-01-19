9d0d8cab4d3912347e315b5592f662bcd93b64da
Lamartune <fatih.tun.2002@gmail.com>
2023-01-19 10:00:28 +0300

    if else tamamlandi.

---
+++ jsDemos/conditionals.js
@@ -0,0 +1,15 @@
+var not = 51
+
+//not 50'den büyükse
+if(not>50){
+    console.log('Geçtiniz.')
+}else if(not>=40){
+    console.log('Bütünlemeye Kaldınız.')
+}
+else{
+    console.log('Kaldınız.')
+}
+
+// if(not<=50){
+//     console.log('Kaldınız.')
+// }
