21fa0106001badd18ec6e0b3dddd433fe0c9d6c5
Lamartune <fatih.tun.2002@gmail.com>
2023-02-01 15:34:02 +0300

    JS Console giris yapildi.

---
+++ JAVASCRIPT/Temeller/Console/app.js
@@ -0,0 +1,15 @@
+console.log("MErhaba")
+console.log(231)
+console.log(true)
+console.log(false)
+
+var a = 20
+
+console.log(a)
+
+console.log(["Ahmet", "Mehmet"])
+
+console.log(typeof a)
+
+console.warn("Bu bir UYARI")
+console.error("Bu sayfa bulunamadı")
