8d389730f2dbc7cad240bedc0cd48c1a4571a877
Lamartune <fatih.tun.2002@gmail.com>
2023-03-15 12:33:45 +0300

    Veri tipleri islendi.

---
+++ JAVASCRIPT/Temeller/Degiskenler Veri Tipleri/app.js
@@ -0,0 +1,140 @@
+// Değişken Oluşturma
+
+// var a = 20;
+
+// var b = 10;
+
+// var c = 40;
+
+// console.log(a,b,c);
+
+//------------------------
+
+//Veri Tipleri
+
+//------------------------
+
+//Primitive-İlkel Veri Tipleri
+
+// var a = 10; //Number Veri Tipi
+// var b = 3.14; //Number Veri Tipi
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+//------------------------
+
+//String Veri Tipi
+
+// var namee = "Fatih";
+
+// console.log(namee);
+// console.log(typeof namee);
+
+//------------------------
+
+//Boolean Veri Tipi
+
+// var a = true;
+
+// console.log(typeof a)
+
+//------------------------
+
+//null Veri Tipi
+
+// var a = null;
+
+// console.log(a);
+
+// console.log(typeof a)
+
+//------------------------
+
+// Undefined Veri Tipi
+
+// var a;
+
+// a =
+
+// console.log(a)
+
+//------------------------
+
+//Reference Veri Tipleri
+
+//------------------------
+
+//Arrays
+
+// var numbers = [1,2,3,4,5,];
+
+// console.log(numbers)
+// console.log(typeof numbers)
+// console.log(numbers[0])
+
+//------------------------
+
+// var person = {
+//     name: "Fatih TÜN",
+//     age: 21
+// }
+
+// console.log(person)
+// console.log(typeof person)
+
+//------------------------
+
+// var date = new Date();
+
+// console.log(date)
+// console.log(typeof date)
+
+//------------------------
+
+// function find_max(nums){
+//     let max_num =
+//     Number.NEGATIVE_INFINITY;
+
+//     for (let num of nums) {
+//         if (num > max_num){
+//             max_num = num
+//         }
+//     }
+//     return max_num
+// }
+
+// console.log(max_num)
+
+//------------------------
+
+// var merhaba = function(){
+//     console.log("Merhaba")
+// }
+
+// console.log(merhaba)
+// console.log(typeof merhaba)
+
+//------------------------
+
+// var a = 10;
+
+// var b = a;
+
+// console.log(a, b);
+
+// a = 20;
+
+// console.log(a, b);
+
+//------------------------
+
+var a = [1,2,3];
+
+var b = a;
+
+console.log(a,b)
+
+a.push(4);
+
+console.log(b)
