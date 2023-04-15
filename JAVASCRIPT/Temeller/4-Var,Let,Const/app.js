b8dc0a3bccde6d21ffc0182dc133adc28b4a7470
Lamartune <fatih.tun.2002@gmail.com>
2023-04-15 16:34:20 +0300

    Veri Tipleri islendi

---
+++ JAVASCRIPT/Temeller/4-Var,Let,Const/app.js
@@ -0,0 +1,28 @@
+//-------------------VAR-------------------
+
+// var name = "Fatih";
+
+// console.log(name);
+
+// name = "CABBAR"
+
+// console.log(name)
+
+//-------------------LET-------------------
+
+let name = "Fatih";
+
+console.log(name);
+
+name = "cabbar";
+
+console.log(name);
+
+//-----------------------------------------
+
+let a , b;
+
+a=10;
+b=20;
+
+console.log(a+b)
