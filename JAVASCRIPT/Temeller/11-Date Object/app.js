f448331066389be812f335cd0e979ffc636f561f
Lamartune <fatih.tun.2002@gmail.com>
2023-06-12 02:15:14 +0300

    Done date obcect.

--- JAVASCRIPT/Temeller/11-Date Object/app.js
+++ JAVASCRIPT/Temeller/11-Date Object/app.js
@@ -1,57 +1,47 @@
 let value;
 
-const programmer = {
-  name: "Fatih Tün",
-  age: 22,
-  email: "tunbusiness1@gmail.com",
-  langs: ["Html, Css, Js"],
+const now = new Date(); //Şu anki zamanı getirir.
+console.log(now);
 
-  adress: {
-    city: "Ankara",
-    street: "Bahçelievler",
-  },
+const date1 = new Date("2002-07-19T17:30:00");
+console.log(date1);
 
-  work: function () {
-    console.log("Programcı Çalışıyor...");
-  },
-};
+const date2 = new Date("July 19 2002");
+console.log(date2);
 
-//Tüm Programmer bilgilerini alma
-value = programmer;
+const date3 = new Date("7/19/2002");
+console.log(date3);
+
+const date4 = new Date(2002, 6, 19, 17, 30, 0);
+console.log(date4);
+
+value = date1.getMonth();
 console.log(value);
 
-//Spesifik Programmer bilgisini alma
-value = programmer.email;
-console.log(value);
-//veya
-value = programmer["email"];
+value = date1.getDate();
 console.log(value);
 
-value = programmer.langs;
+value = date1.getDay();
 console.log(value);
 
-value = programmer.adress;
+value = date1.getHours();
 console.log(value);
 
-value = programmer.adress.city;
+value = date1.getMinutes();
 console.log(value);
 
-value = programmer.adress.street;
+value = date1.getSeconds();
 console.log(value);
 
-//Fonksiyonu kullanma işlemi
-programmer.work();
-
-const programmers = [
-  { name: "Fatih", age: 22 },
-  { name: "Cabbar", age: 34 },
-];
-
-value = programmers[0];
+value = date1.getMilliseconds();
 console.log(value);
 
-value = programmers[0].name;
-console.log(value);
+date1.setMonth(9);
+date1.setDate(12);
+date1.setFullYear(1999);
+date1.setHours(22);
+date1.setMinutes(15);
+date1.setSeconds(33);
 
-value = programmers[1].age;
-console.log(value);
+value = date1;
+console.log(date1);
